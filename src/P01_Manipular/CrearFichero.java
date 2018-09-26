1937d6eb57858e206d73aeb5fcfbb4ce9ce69d0d
ifc <ifc@DESKTOP-B9E6QR2>
2018-09-26 19:08:18 +0200

    Ejercicios iniciales con ficheros

---
+++ src/P01_Manipular/CrearFichero.java
@@ -0,0 +1,23 @@
+package P01_Manipular;
+
+import java.io.File;
+import java.io.FileWriter;
+
+public class CrearFichero {
+
+	public static void main(String[] args) {
+		
+		try {
+		//Creamos ficheros de texto en ifc
+		FileWriter f1 = new FileWriter("C:\\Users\\ifc\\f1.txt");
+		FileWriter f2 = new FileWriter("C:\\Users\\ifc\\f2.txt");
+		
+		} catch (Exception ex) {
+			
+			System.out.println(ex);
+		
+		}
+
+	}
+
+}
---
+++ src/P02_Metodos/Metodos.java
@@ -0,0 +1,29 @@
+package P02_Metodos;
+
+import java.io.FileWriter;
+
+public class Metodos {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+		FileWriter f1 = new FileWriter("C:\\Users\\ifc\\f1.txt");
+		
+		//Escribimos en el archivo f1
+		f1.write("El cipotegato es en Agosto");
+		f1.close(); 
+				
+		System.out.println(f1.getEncoding()); //metodo de ejemplo
+	    
+	    
+
+		} catch (Exception ex) {
+			
+			System.out.println(ex);
+		
+
+	   }
+
+	}
+}
