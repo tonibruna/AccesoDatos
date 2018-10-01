ff37eda0a38a3c2f1aa01ef5bb4908b2faee64db
ifc <ifc@DESKTOP-B9E6QR2>
2018-10-01 17:47:33 +0200

    Crear un fichero con la clase File

--- src/P01_Manipular/CrearFichero.java
+++ src/P01_Manipular/CrearFichero.java
@@ -1,25 +1,22 @@
 package P01_Manipular;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.IOException;
 
 public class CrearFichero {
 
 	public static void main(String[] args) {
 		
-		try {
-		//Creamos ficheros de texto en ifc
-		FileWriter f1 = new FileWriter("C:\\Users\\ifc\\f1.txt");
+		File f1=new File("C:\\Users\\ifc\\Desktop\\f1.txt");
 		
-		
-		FileWriter f2 = new FileWriter("C:\\Users\\ifc\\f2.txt");
-		
-		} catch (Exception ex) {
-			
-			System.out.println(ex);
-		
+		if(f1.exists()) {
+			System.out.println("El fichero ya existe");
+		}else {
+			try {
+				f1.createNewFile();
+			}catch(IOException e){
+				System.out.println(e.getMessage());
+			}			
 		}
-
 	}
-
 }
