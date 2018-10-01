786c5edd34cd7321d319175b33d414ec30e4695a
ifc <ifc@DESKTOP-B9E6QR2>
2018-10-01 17:43:54 +0200

    Leemos una linea entera con BufferedReader

---
+++ src/P03_LecturaEscritura/LeerLinea.java
@@ -0,0 +1,31 @@
+package P03_LecturaEscritura;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class LeerLinea {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		//Con esto leemos una linea completa, en vez de caracter por caracter
+
+		File f4=new File("C:\\Users\\ifc\\eclipse-workspace\\LeerLinea.txt");
+		
+		FileReader fichero = new FileReader (f4);
+		
+		BufferedReader br= new BufferedReader(fichero);
+		
+		String tmp;
+		
+		while ((tmp=br.readLine())!=null) {
+			System.out.println(tmp);
+		}
+		
+		br.close();
+	}
+
+}
