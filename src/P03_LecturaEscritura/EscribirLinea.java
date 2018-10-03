38078b273038cad1e1440fc09d8898f89e641a65
ifc <ifc@DESKTOP-B9E6QR2>
2018-10-03 18:40:51 +0200

    Escribir Lineas con Buffered

---
+++ src/P03_LecturaEscritura/EscribirLinea.java
@@ -0,0 +1,43 @@
+package P03_LecturaEscritura;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class EscribirLinea {
+
+	public static void main(String[] args) {
+		
+		try {
+		
+		File f5=new File("C:\\Users\\ifc\\eclipse-workspace\\LeerLinea.txt");
+		
+		FileWriter fichero = new FileWriter (f5);
+		
+		BufferedWriter br= new BufferedWriter(fichero);
+		
+		int nLineas = 10;
+		
+		for(int i=1;i<=nLineas;i++) {
+			    br.write("Fila numero" + i);
+			    br.newLine();
+			
+		}
+		br.close();
+		
+	}
+	catch (FileNotFoundException fn) {
+		System.out.println("El fichero no se encuentra...");
+	}
+	
+	catch (IOException io) {
+		System.out.println("Error de E/S");
+	}
+	
+   }
+
+}
+
+
