efed2ac95e278ae0a50893937fbca6243374d71b
antonio bruna <joseantonio.bruna.delcarmen@iestubalcain.net>
2018-11-05 16:31:27 +0100

    Cambiado el titulo de la clase

---
+++ src/XML_Manejo/Ej1_Crear_Cancion.java
@@ -0,0 +1,119 @@
+package XML_Manejo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+
+public class Ej1_Crear_Cancion {
+
+	public static void main(String[] args) throws IOException{
+		
+		 //Creamos variable de fichero y flujo de lectura de objetos
+		
+		  File f = new File ("C:\\Users\\ifc\\eclipse-workspace\\AD_01_Ficheros\\src\\XML_Manejo\\Canciones.dat");
+	 	  
+		  FileInputStream fileIn = new FileInputStream(f);
+		  ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+		  
+		 //Creamos variable
+		  
+		  Cancion cancion = new Cancion();
+		   
+		 // Instancia para construir el parser
+		   
+		 DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
+		  
+		 try {
+		
+			//Para construir el documento XML
+			 
+		   DocumentBuilder builder = factory.newDocumentBuilder();
+		     
+		   DOMImplementation implementation = builder.getDOMImplementation();
+		   
+		   Document document = 
+				   implementation.createDocument(null, "Canciones", null);
+		         document.setXmlVersion("1.0"); 
+		   
+		     for(int i=0;i<5;i++) {
+		    	 
+		    	 	 //leemos objeto cancion
+				     cancion = (Cancion) objectIn.readObject();
+		    	 
+				     //creamos nodo raiz 
+					 Element raiz = document.createElement("Cancion");
+					 //añadimos nodo principal
+			         document.getDocumentElement().appendChild(raiz); 
+			        
+			         //Creacion de elementos
+			         // ID                       
+			         CrearElemento("id",Integer.toString(cancion.getId()), raiz, document); 
+			         // Año
+			         CrearElemento("anyo",Integer.toString(cancion.getAnyo()), raiz, document); 
+			         // Titulo
+			         CrearElemento("titulos",cancion.getTitulo(), raiz, document); 
+			         // Artista
+			         CrearElemento("artista",cancion.getArtista(), raiz,document); 
+			         //Cancion Española
+			         CrearElemento("cancion_española",Boolean.toString(cancion.getCancionEspañola()), 
+			        		 raiz, document); 
+				 }	
+				
+		     Source source = new DOMSource(document);
+		     
+		     Result result = 
+		            new StreamResult(new java.io.File("C:\\Users\\ifc\\eclipse-workspace\\AD_01_Ficheros\\src\\XML_Manejo\\Canciones.dat"));  
+		     
+		     Transformer transformer =
+		            TransformerFactory.newInstance().newTransformer();
+		     
+		     //se trasforma el documento al fichero
+		     transformer.transform(source, result);
+
+		    
+		    // MOSTRAR EL DOCUMENTO POR CONSOLA
+		    Result console = new StreamResult(System.out);
+		    transformer.transform(source, console);	   
+			   
+		    }catch(Exception e)
+		 { 
+		    	System.out.println("Ha habido un error: " + e); 
+		 }
+		    
+		 objectIn.close(); 
+		 
+ } //fin de main
+
+//Insercion de los datos del empleado
+	/**
+	 * Metodo que crea un elemento para escribirlo en documento xml
+	 * @param datoCancion Elemento de la canción
+	 * @param valor Valor que se va a dar al elemento
+	 * @param raiz Elemento raiz
+	 * @param document Documento donde se van a añadir estos elementos
+	 */
+static void  CrearElemento(String datoCancion, String valor,
+                           Element raiz, Document document)
+{
+   Element elem = document.createElement(datoCancion); 
+   Text text = document.createTextNode(valor); //damos valor
+   raiz.appendChild(elem); //pegamos el elemento hijo a la raiz
+   elem.appendChild(text); //pegamos el valor al elemento hijo	 	
+ }
+
+}//fin de la clase
