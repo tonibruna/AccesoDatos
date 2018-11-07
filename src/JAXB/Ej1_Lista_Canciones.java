d4a3e943e66a88b67f5d2c1d385cb61ae96401ab
antonio bruna <joseantonio.bruna.delcarmen@iestubalcain.net>
2018-11-07 19:35:42 +0100

    sin terminar

--- src/JAXB/Ej1_Lista_Canciones.java
+++ src/JAXB/Ej1_Lista_Canciones.java
@@ -1,5 +1,10 @@
 package JAXB;
 
+import java.util.ArrayList;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
 public class Ej1_Lista_Canciones {
 
 }
