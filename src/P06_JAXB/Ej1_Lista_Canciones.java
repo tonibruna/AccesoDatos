a06c7ca2b4e7c526339164c2be729af7092cb39a
antonio bruna <joseantonio.bruna.delcarmen@iestubalcain.net>
2018-11-12 17:28:24 +0100

    Clase Lista de canciones terminada

--- src/P06_JAXB/Ej1_Lista_Canciones.java
+++ src/P06_JAXB/Ej1_Lista_Canciones.java
@@ -1,10 +1,45 @@
+//Clase que contiene una lista de canciones
+
 package P06_JAXB;
 
 import java.util.ArrayList;
+import java.util.List;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
+//namespace es opcional
+//@XmlRootElement(namespace = "ejemplo1.xml")
+//@XmlRootElement(namespace = "")
+
+//Esto significa que la clases "Libreria.java" es el elemento raiz
+@XmlRootElement()
+
 public class Ej1_Lista_Canciones {
+	
+	private ArrayList<Ej1_Cancion_JABX> lista = new ArrayList<Ej1_Cancion_JABX>();
+	
+	public Ej1_Lista_Canciones()
+	{
+		
+	}
+	
+	public void setListaCanciones(ArrayList<Ej1_Cancion_JABX> vlista)
+	{
+		lista = vlista;
+	}
+	
+	//-Wrapper, envoltura alrededor la representación XML
+    //Anotaciones para atributos que son colecciones
+	//-Hay que poner estas anotaciones delante del metodo
+	//que devuelve la coleccion
+	
+    @XmlElementWrapper(name = "ListaCanciones") 
+    @XmlElement(name = "Cancion")
+	public List<Ej1_Cancion_JABX> getListaCanciones()
+	{
+		return lista;
+	}
 
 }
