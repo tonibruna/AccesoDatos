b8968f922d26ccb1e03c74b784f5810f7c9151a6
antonio bruna <joseantonio.bruna.delcarmen@iestubalcain.net>
2018-11-19 17:38:45 +0100

    Crear base datos SQLite

--- UD2_CONECTORES/Ejercicios/Crear_BaseDatos_SQLite.java
+++ UD2_CONECTORES/Ejercicios/Crear_BaseDatos_SQLite.java
@@ -1,5 +1,6 @@
 package Ejercicios;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,6 +9,9 @@ import java.sql.Statement;
 
 
 public class Crear_BaseDatos_SQLite {
+	
+	static Statement statement;
+	static ResultSet rs;
 
 	public static void main(String[] args) {
 		
@@ -17,28 +21,32 @@ public class Crear_BaseDatos_SQLite {
 		  
 		// Crear una conexion de base de datos
           connection = DriverManager.getConnection
-          ("jdbc:sqlite:\\ACCESO_DATOS_DAM_2\\UD2_CONECTORES\\Ejercicios\\databaseSQLite.db");
+          ("jdbc:sqlite:C:\\Users\\ifc\\eclipse-workspace\\ACCESO_DATOS_DAM_2\\UD2_CONECTORES\\SQLite\\SQLite\\datos.db");
           
-          Statement statement = connection.createStatement();
+          statement = connection.createStatement();
           statement.setQueryTimeout(30);  // tiempo para ejecutar el objeto
           
           //Crear tabla
-          statement.executeUpdate("drop table if exists person");
+          statement.executeUpdate("drop table if exists persona");
           
           statement.executeUpdate("create table persona (id integer, nombre string,"
           		+ "apellido string, edad interger, telefono interger, nacionalidad string)");
           
-          statement.executeUpdate("insert into persona values(1, 'leo', 'perez', 32, 976234567, española)");
+          statement.executeUpdate("insert into persona values(1, 'leo', 'perez', 32, 976234567, 'espanola')");
           
-          statement.executeUpdate("insert into persona values(2, 'julia', 'marquez, 42, 987465213, española)");
+          statement.executeUpdate("insert into persona values(2, 'julia', 'marquez', 42, 987465213, 'espanola')");
           
           //para ejecutar la consulta
-          ResultSet rs = statement.executeQuery("select * from persona");
+          rs = statement.executeQuery("select * from persona");
           while(rs.next())
           {
         	  // leer el resultado
-              System.out.println("name = " + rs.getString("name"));
-              System.out.println("id = " + rs.getInt("id"));
+        	  System.out.println("id = " + rs.getInt("id"));
+              System.out.println("nombre = " + rs.getString("nombre"));
+              System.out.println("apellido = " + rs.getString("apellido"));
+              System.out.println("edad = " + rs.getInt("edad"));
+              System.out.println("telefono = " + rs.getInt("telefono"));
+              System.out.println("nacionalidad = " + rs.getString("nacionalidad"));
             }
           }
           catch(SQLException e)
@@ -60,6 +68,33 @@ public class Crear_BaseDatos_SQLite {
            System.err.println(e.getMessage());
          }
        }
-     }
+	}
+	
+	public static void insertar(int id, String nombre, String apellido, 
+			  int edad, int telefono, String nacionalidad) throws SQLException {
+		  
+		statement.executeUpdate("insert into persona values("+id+", 'leo', 'perez', 32, 976234567, 'espanola')");
+		  
+		  
+	  }
+	  
+    /*  public static consultar() throws SQLException{
+    	   
+    	   ResultSet rs = statement.executeQuery("select * from persona");
+           while(rs.next())
+           {
+         	  // leer el resultado
+         	  System.out.println("id = " + rs.getInt("id"));
+               System.out.println("nombre = " + rs.getString("nombre"));
+               System.out.println("apellido = " + rs.getString("apellido"));
+               System.out.println("edad = " + rs.getInt("edad"));
+               System.out.println("telefono = " + rs.getInt("telefono"));
+               System.out.println("nacionalidad = " + rs.getString("nacionalidad"));
+             }
+       }
+
+     }*/
    }
  
+
+
